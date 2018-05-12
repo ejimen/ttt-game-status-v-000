c4b6c071c8044782fcf0a3b2fa11f789fefb5a18
Elizabeth Jimenez <lizliz123@gmail.com>
2018-05-12 03:58:59 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -43,9 +43,7 @@ end
 def draw?(board)
   if !won?(board) && full?(board)
     return true
-  elsif !won?(board) && !full?(board)
-    return false
-  else won?(board)
+  elsif !won?(board) && !full?(board) || !won?(board)
     return false
   end
 end
